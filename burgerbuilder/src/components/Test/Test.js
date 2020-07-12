2d9c1173a0244e52080b8fc70b5acaeb6aee4907
Fabio Apolonio <fabioapolonio@brq.com>
2020-07-12 11:13:25 -0300

    static ingredients

---
+++ burgerbuilder/src/components/Test/Test.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Test.module.css'
+
+const Test = (props) => {
+    return (
+        <div className={classes.Meat}>
+            
+        </div>
+    );
+};
+
+export default Test;
