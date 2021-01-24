52b05f7dd8cb6da4696a00acde989153d7bfe825
fabio <fabiocosta07@gmail.com>
2021-01-24 16:32:28 -0300

    class 84 - mongo example

---
+++ jscourse/section8/83-mondodb-and-mongoose/app.js
@@ -0,0 +1,31 @@
+var mongoose = require('mongoose')
+
+mongoose.connect('mongodb+srv://admin:admin123@cluster0.qeqlg.mongodb.net/test?retryWrites=true&w=majority')
+
+var Schema = mongoose.Schema;
+
+var personSchema = new Schema({
+    firstname: String
+})
+
+var Person = mongoose.model('Person', personSchema);
+
+var john = Person({
+    firstname: 'John'
+})
+
+john.save(function(err){
+    if (err) throw err
+
+    console.log('person saved')
+})
+
+Person.find({_id:'600dc62be30853cf6ac9559c'},(err, users)=>{
+    if (err) throw err;
+    console.log(users)
+})
+
+Person.find({},(err, users)=>{
+    if (err) throw err;
+    console.log(users)
+})
