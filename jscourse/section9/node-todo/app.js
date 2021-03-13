1290872a69d373160ad38e689d15581bc281eead
fabio <fabiocosta07@gmail.com>
2021-03-13 15:29:27 -0300

    class 95

--- jscourse/section9/node-todo/app.js
+++ jscourse/section9/node-todo/app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var app = express();
 var config  = require('./config');
 var setupController = require('./controllers/setupControllers')
+var apiController = require('./controllers/apiController')
+
 var mongoose = require('mongoose');
 
 var port = process.env.PORT || 3000;
@@ -12,5 +14,6 @@ app.set('view engine', 'ejs');
 
 mongoose.connect(config.getDbConnectionString());
 setupController(app);
+apiController(app);
 
 app.listen(port);
