82353cd145e119609e3e5ca9014f10768bd84e15
fabio <fabiocosta07@gmail.com>
2021-05-12 20:21:00 -0300

    bubble sort

---
+++ sorting/bubblesort/main.js
@@ -0,0 +1,18 @@
+const numbers = [ 87, 283, 4, 0];
+
+function bubbleSort(array) {
+    for(j=0; j < array.length; j++) {
+        for(i=0;(i+1)< array.length; i++){
+            if (array[i] > array[i+1]) {
+                let v = array[i+1];
+                array[i+1] = array[i]
+                array[i] = v
+                allsorted = false
+            }
+        }    
+    }
+    
+}
+
+bubbleSort(numbers);
+console.log(numbers);
