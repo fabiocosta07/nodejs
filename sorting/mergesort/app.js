f33e926ab5457032159ab5c8c43ca901dd402245
fabio <fabiocosta07@gmail.com>
2021-07-03 14:49:30 -0300

    merge sort

--- sorting/mergesort/app.js
+++ sorting/mergesort/app.js
@@ -1,4 +1,4 @@
-const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0];
+const numbers = [99, 44, 6, 2, 1, 5, 63, 87, 283, 4, 0,4];
 
 function mergeSort (array) {
   if (array.length === 1) {
@@ -13,25 +13,34 @@ function mergeSort (array) {
   )
 }
 
-function merge(left, right){
-  var ar = []
-  var j = 0
-  for(i=0; i < left.length; i++)  {
-    for (;j < right.length; j++) {
-      if (right[j] <= left[i]) {        
-        ar.push(right[j])
+function merge(left, right) {
+  var r = []
+  var i =0
+  var j =0
+  while (i < left.length &&
+      j < right.length) {
+      if (left[i] < right[j]) {
+        r.push(left[i])
+        i++      
       } else {
-        break;
-      }                         
-    }
-    ar.push(left[i])
-  }
-  for (;j < right.length; j++) {
-    ar.push(right[j])
+        r.push(right[j])
+        j++      
+      }
+  } 
+  if (i < left.length){
+    r = r.concat(left.slice(i))
   }  
-  return ar
+  if (j < right.length){
+    r = r.concat(right.slice(j))
+  }  
+  return r;
 }
 
+const test = [3,4,4,7]
+const test2 = [5,6]
+
+var res = merge(test, test2)
+console.log(res);
 
 const answer = mergeSort(numbers);
 console.log(answer);
