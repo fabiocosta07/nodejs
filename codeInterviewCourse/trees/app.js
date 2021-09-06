279950d52ea62868bb8055dca4a7a52c782a0361
fabio <fabiocosta07@gmail.com>
2021-09-06 08:45:26 -0300

    trees

---
+++ codeInterviewCourse/trees/app.js
@@ -0,0 +1,48 @@
+class Node {
+    constructor(value) {
+        this.left = null
+        this.right = null
+        this.value = value
+    }    
+}
+
+class BinarySearchTree {
+    constructor() {
+        this.root = null
+    }
+    insert(value){
+        const newNode = new Node(value)
+        if (!this.root) {
+            this.root = newNode
+            return
+        } 
+        let cursorNode;
+    }
+    lookup(value) {
+
+    }
+    remove(value) {
+
+    }
+}
+
+const tree = new BinarySearchTree();
+tree.insert(9)
+tree.insert(4)
+tree.insert(6)
+tree.insert(20)
+tree.insert(170)
+tree.insert(15)
+tree.insert(1)
+JSON.stringify(traverse(tree.root))
+
+//     9
+//  4     20
+//1  6  15  170
+
+function traverse(node) {
+  const tree = { value: node.value };
+  tree.left = node.left === null ? null : traverse(node.left);
+  tree.right = node.right === null ? null : traverse(node.right);
+  return tree;
+}
